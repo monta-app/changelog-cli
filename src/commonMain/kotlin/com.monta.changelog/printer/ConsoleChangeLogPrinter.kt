c7b97fba15b624a648e7c1ef4bf8e170741b0b8e
Brian Estrada <lovesguitar@gmail.com>
2022-05-22 11:06:23 +0200

    fix: added publish token permissions

--- src/commonMain/kotlin/com.monta.changelog/printer/ConsoleChangeLogPrinter.kt
+++ src/commonMain/kotlin/com.monta.changelog/printer/ConsoleChangeLogPrinter.kt
@@ -2,8 +2,43 @@ package com.monta.changelog.printer
 
 import com.monta.changelog.model.ChangeLog
 import com.monta.changelog.util.LinkResolver
+import com.monta.changelog.util.MarkdownFormatter
+import com.monta.changelog.util.resolve
 
 class ConsoleChangeLogPrinter : ChangeLogPrinter {
     override suspend fun print(linkResolvers: List<LinkResolver>, changeLog: ChangeLog) {
+        println(
+            buildString {
+                changeLog.groupedCommitMap.forEach { (scope, commitsGroupedByType) ->
+                    if (scope != null) {
+                        appendLine()
+                        append(
+                            MarkdownFormatter.GitHub.header(
+                                (scope).replaceFirstChar { char ->
+                                    char.uppercaseChar()
+                                }
+                            )
+                        )
+                        appendLine()
+                    }
+                    commitsGroupedByType.forEach { (type, commits) ->
+                        append(
+                            MarkdownFormatter.GitHub.title("${type.emoji} ${type.title}")
+                        )
+                        commits.forEach { commit ->
+                            append(
+                                MarkdownFormatter.GitHub.listItem(
+                                    linkResolvers.resolve(
+                                        markdownFormatter = MarkdownFormatter.GitHub,
+                                        message = commit.message
+                                    )
+                                )
+                            )
+                        }
+                    }
+                    appendLine()
+                }
+            }
+        )
     }
 }
