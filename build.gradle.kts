c2dec89e6aee4fa6beb7f7e714cff9e5bd3d297c
Morten Andersen <ma@monta.app>
2023-05-23 14:49:40 +0200

    chore: Upgrade io.kotest.multiplatform 5.6.1 -> 5.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform") version "1.8.21"
     kotlin("plugin.serialization") version "1.8.21"
-    id("io.kotest.multiplatform") version "5.6.1"
+    id("io.kotest.multiplatform") version "5.6.2"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
 }
 
