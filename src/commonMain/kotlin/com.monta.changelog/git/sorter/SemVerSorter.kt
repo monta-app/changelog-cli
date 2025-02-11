17de682b3d1fd0f65933a57e01e3330ea7232754
Morten Andersen <ma@monta.app>
2025-02-11 10:20:19 +0100

    fix: Fix the unstable semver sorting which lead to wrong release notes [ESP-13]
    * fixed the small warning/bug if 'slack-channel' was left empty (the library-kotlin uses the version supporting multiple channels 'slack-channels'
    * added dependency on [kotlin-semver](https://github.com/z4kn4fein/kotlin-semver)
    * added test cases for the semver sorter
    
    JIRA [ESP-13](https://montaapp.atlassian.net/browse/ESP-13)

--- src/commonMain/kotlin/com.monta.changelog/git/sorter/SemVerSorter.kt
+++ src/commonMain/kotlin/com.monta.changelog/git/sorter/SemVerSorter.kt
@@ -1,26 +1,28 @@
 package com.monta.changelog.git.sorter
 
 import com.monta.changelog.git.getTagValue
+import com.monta.changelog.util.DebugLogger
+import io.github.z4kn4fein.semver.Version
 
 class SemVerSorter : TagSorter {
 
     override fun sort(tags: List<Tag>): List<Tag> {
         return tags.mapNotNull { tag ->
             val shortTag = tag.shortTag
-            // Get the last part of the tag so stuff like releases/tag/2023-02-28-14-39 can still be valid
+            // Get the last part of the tag so stuff like releases/tag/v1.2.3 can still be valid
             var tagValue = shortTag.getTagValue()
             // Remove the V portion if it's there (Not required for sorting)
             tagValue = tagValue.replace("v", "")
-            // We check it's a valid tag by seeing it contains three dots (there is 100% a better way to do this)
-            if (shortTag.count { it == '.' } >= 2) {
-                tagValue to tag
-            } else {
+            try {
+                Version.parse(tagValue) to tag
+            } catch (e: Exception) {
+                DebugLogger.warn("Failed to parse tag '$tagValue' - ignored")
                 null
             }
         }
             // Sort by the date
-            .sortedByDescending { (localDateTime, _) ->
-                localDateTime
+            .sortedByDescending { (shortTag, _) ->
+                shortTag
             }
             // Return only the tag
             .map { (_, tag) ->
