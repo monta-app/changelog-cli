34bb2a30669ca7ff2ff0dbfb92b1e10711c1e078
Jesper Terkelsen <jete@monta.com>
2026-01-12 19:27:52 -0500

    fix: remove unused import in LinkResolverTest

--- src/commonTest/kotlin/com/monta/changelog/util/LinkResolverTest.kt
+++ src/commonTest/kotlin/com/monta/changelog/util/LinkResolverTest.kt
@@ -1,7 +1,6 @@
 package com.monta.changelog.util
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldNotContain
 
